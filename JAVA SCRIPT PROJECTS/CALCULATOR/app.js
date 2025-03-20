45094cdcdff764794f2bd42073d175bc04d14423
pretender-2905 <pretender2905@gmail.com>
2025-03-20 12:52:34 +0500

    calculator done

--- JAVA SCRIPT PROJECTS/CALCULATOR/app.js
+++ JAVA SCRIPT PROJECTS/CALCULATOR/app.js
@@ -1,8 +1,22 @@
 // Event listner for removing placeholder whne clicked on input field
 
 // let input = document.querySelector(".input").placeholder;
-let input = document.getElementById("input");
+let  = document.getElementById("input");
 input.addEventListener("click", ()=>{
     input.placeholder = ""
 })
 
+function appendToDisplay(input){
+    let display = document.getElementById("display");
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+
+function calculate(){
+    let expression = display.value.replace(/÷/g, '/').replace(/x/g, '*');
+    display.value = eval(expression);
+}
