2dc415ef338b0a3641c86178252ccb0ae794c3e8
pretender-2905 <pretender2905@gmail.com>
2025-03-22 02:26:29 +0500

    to do list completed

--- JAVA SCRIPT PROJECTS/TO DO LIST/app.js
+++ JAVA SCRIPT PROJECTS/TO DO LIST/app.js
@@ -1,9 +1,31 @@
 let input = document.getElementById("input");
-
-function addTask(){
+let taskContainer = document.querySelector(".get-task-div");
+function addTask() {
     let task = input.value;
-    let row = document.querySelector(".get-task-div");
+    if(task === ""){
+        return;
+    }
 
+let task_btn_div = document.createElement("div");
+task_btn_div.classList.add("task-btn-div"); //for styling
 
-    row.innerHTML = task;
+let task_p = document.createElement("p");
+let task_button = document.createElement("button");
+task_p.textContent = `➼ ${task}`;
+task_button.textContent = `Delete`;
+
+task_button.classList.add("delete-btn-style");
+task_p.classList.add("task-p");
+
+taskContainer.appendChild(task_btn_div);
+task_btn_div.appendChild(task_p);
+task_btn_div.appendChild(task_button);
+
+// delete functionality
+
+task_button.onclick = function(){
+    task_btn_div.remove();
+}
+
+input.value = ""
 }
